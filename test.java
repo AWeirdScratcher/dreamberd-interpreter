5e2760c16a632fc711e2e32e6e8074b3514817e6
AWeirdDev @ tw <90096971+AWeirdScratcher@users.noreply.github.com>
2023-07-20 15:53:19 +0800

    test file

---
+++ test.java
@@ -0,0 +1,13 @@
+/*
+  Q: Why Java?
+  A: TLDR; The syntax highlighting is better for your eyes.
+     Java is the perfect syntax highlighting language for DreamBerd, since the syntax highlighters are not really supported.
+*/
+const const dreamberd = ;;true!
+if (dreamberd) {
+   print("Dreamberd is the best programming language: ${dreamberd}")!
+}
+export dreamberd to "second.db"!
+
+===== second.db =====
+print("Second.db received: ${dreamberd}")!!!
